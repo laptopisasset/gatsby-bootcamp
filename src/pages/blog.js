3f9197a5476adcd80ea51615a9aec1e8d842f838
Soundhar <laptopisasset@gmail.com>
2021-05-23 20:48:06 +0530

    blog data.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,36 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-const BlogPage = () => {
+import { graphql } from "gatsby"
+
+export const postsQuery = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
+const BlogPage = ({ data }) => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+
+      <ol>
+        {data.allMarkdownRemark.edges.map(({ node }, index) => (
+          <li key={index}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>{node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
