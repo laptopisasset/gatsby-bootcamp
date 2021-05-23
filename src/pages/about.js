c60692f5a2c81895d506551a042357f4dbe63566
Soundhar <laptopisasset@gmail.com>
2021-05-23 18:30:55 +0530

    Added the Layout component.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>About Me</h1>
       <p>I currently work as full-stack developer.</p>
       <p>
         <Link to="/contact">Want to work with me? Reach out.</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Footer from "../components/footer"
-
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Hello,</h1>
       <h2>
         I'm Soundhar, a full-stack developer living in beautiful Coimbatore
@@ -16,8 +13,7 @@ const IndexPage = () => {
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
