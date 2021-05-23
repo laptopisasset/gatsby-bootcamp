983b5de81673da203ce1392caebbb2b16cbd5d0a
Soundhar <laptopisasset@gmail.com>
2021-05-23 17:44:27 +0530

    Added the contact and about pages.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This page describes the profile and works of me.</p>
+    </div>
+  )
+}
+
+export default AboutPage
