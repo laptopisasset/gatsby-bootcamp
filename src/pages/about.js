ec0578bbdb35e122a3076f200110a07065cff7c1
Soundhar <laptopisasset@gmail.com>
2021-05-23 18:08:58 +0530

    Added Link from about to contact and added link to twitter handle.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About Me</h1>
       <p>I currently work as full-stack developer.</p>
+      <p>
+        <Link to="/contact">Want to work with me? Reach out.</Link>
+      </p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,13 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>The best way to reach me is via email laptopisasset@gmail.com</p>
+      <p>
+        The best way to reach me is via{" "}
+        <a href="https://twitter.com/@SoundharNatara1" target="_blank">
+          @SoundharNatara1
+        </a>
+        on Twitter
+      </p>
     </div>
   )
 }
