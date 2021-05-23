e5398c4e44e36b2b99ed1639f40d96ad8f92f640
Soundhar <laptopisasset@gmail.com>
2021-05-23 20:22:22 +0530

    gatsby-source-file-system.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,14 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Soundhar Natarajan",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
